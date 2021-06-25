d59a1d0bf21608f17e2ccec164c5394acba1b77b
OrahKokos <orah@kokos.tech>
2021-06-25 12:57:20 +0000

    initial type implementation

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -21,17 +21,27 @@ type PositiveStatus = PaymentCompleteStatus;
 
 type CoinpaymentIPNStatus = NegativeStatus | NeutralStatus | PositiveStatus;
 
+enum IPN_TYPES {
+  SIMPLE = 'simple',
+  BUTTON = 'button',
+  DONATION = 'donation',
+  CART = 'cart',
+  API = 'api',
+  DEPOSIT = 'deposit',
+  WITHDRAWAL = 'withdrawal',
+}
+
 interface BaseIPN extends ParsedUrlQueryInput {
   ipn_version: string;
   ipn_mode: 'hmac';
   ipn_id: string;
   merchant: string;
-  status: CoinpaymentIPNStatus;
-  status_text: string;
-  txn_id: string;
 }
 
 interface withCommon {
+  status: CoinpaymentIPNStatus;
+  status_text: string;
+  txn_id: string;
   send_tx: string;
   received_amount: string;
   received_confirms: string;
@@ -42,7 +52,13 @@ interface with2Currency {
   currency2: string;
   amount1: string;
   amount2: string;
+}
+
+interface withSubtotal {
   subtotal: string;
+}
+
+interface withSingleItemOptions {
   on1: string;
   ov1: string;
   on2: string;
@@ -65,6 +81,10 @@ interface withNet {
 interface withItemAmount {
   item_amount: string;
 }
+interface withItemDesc {
+  item_desc: string;
+}
+
 interface withItemInfo {
   item_name: string;
   item_number: string;
@@ -79,120 +99,126 @@ interface withExtra {
   extra: string;
 }
 
-type CommonGroup1 = with2Currency & withShippingFee & withTax & withFee;
-
-type CommonGroup12 = withItemAmount;
-
-type CommonGroup13 = withItemInfo;
-
-type SimpleIPN = BaseIPN &
-  with2Currency &
-  withShippingFee &
-  withTax &
-  withFee &
-  withItemInfo &
-  withInvoice &
-  withCustom &
-  withNet &
-  withCommon;
-
-interface SimpleIPNz extends BaseIPN {
-  // CommonGroup12
-
-  // withCommon;
-  type: 'simple';
-  item_desc: string;
-}
-interface ButtonIPN extends BaseIPN {
-  // CommonGroup12
-  // withItemInfo &
-  // withInvoice &
-  // withCustom &
-  // withCommon;
-  // withNet &
-  // withExtra
-  type: 'button';
-  quantity: string;
-}
-interface DonationIPN extends BaseIPN {
-  // CommonGroup1
-  // withItemInfo
-  // withInvoice
-  // withCustom
-  // withCommon
-  // withExtra
-  // withNet
-  type: 'donation';
-}
-interface CartIPN extends BaseIPN {
-  // CommonGroup1
-  // withInvoice
-  // withCustom
-  // withCommon
-  // withExtra
-  type: 'cart';
-  item_name_1: string;
-  item_amount_1: string;
-  item_quantity_1: string;
-  item_number_1: string;
-  item_on1_1: string;
-  item_ov1_1: string;
-  item_on2_1: string;
-  item_ov2_1: string;
-}
-
-// -----------------
-interface ApiIPN extends BaseIPN {
-  // with2Currency
-  // withFee
-  // withItemInfo
-  // withInvoice
-  // withCustom
-  // withCommon
-  type: 'api';
+interface withBuyerInfo {
   buyer_name: string;
   email: string;
 }
 
-interface DepositIPN extends BaseIPN {
-  type: 'deposit';
-  address: string;
-  dest_tag?: string;
-  currency: string;
-  confirms: number;
-  amount: string;
-  amounti: string;
-  fee?: string; // Only positive
-  feei?: string; // Only positive
-  fiat_coin: string; // The ticker code of the fiat currency you selected on the Merchant Settings tab of the Account Settings page (USD, EUR, etc.) Make sure to check this for accuracy for security in your IPN handler! Huh?
-  fiat_amount: string;
-  fiat_amounti: string;
-  fiat_fee?: string; // Only positive
-  fiat_feei?: string; // Only positive
-  label?: string;
-}
-interface WithdrawalIPN extends BaseIPN {
-  type: 'withdrawal';
-  id: string;
-  address: string;
-  currency: string;
-  amount: string;
-  amounti: string;
-}
-
-interface BuyerIPN {
+interface withBuyerInfoFull {
   first_name: string;
   last_name: string;
   company: string;
   email: string;
 }
-interface BuyerWithShippingFeeAddressIPN {
-  address1: string;
-  address2: string;
-  city: string;
-  state: string;
-  zip: string;
-  country: string;
-  country_name: string;
-  phone: string;
+
+interface withBuyerShippingAddress {
+  address1?: string;
+  address2?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+  country?: string;
+  country_name?: string;
+  phone?: string;
 }
+interface withCurrency {
+  address: string;
+  currency: string;
+  amount: string;
+  amounti: string;
+}
+
+type SimpleIPN = BaseIPN & { type: IPN_TYPES.SIMPLE } & with2Currency &
+  withSubtotal &
+  withSingleItemOptions &
+  withShippingFee &
+  withTax &
+  withFee &
+  withNet &
+  withItemInfo &
+  withItemAmount &
+  withItemDesc &
+  withInvoice &
+  withCustom &
+  withCommon &
+  withBuyerInfoFull &
+  withBuyerShippingAddress;
+
+type ButtonIPN = BaseIPN & { type: IPN_TYPES.BUTTON } & with2Currency &
+  withSubtotal &
+  withSingleItemOptions &
+  withShippingFee &
+  withTax &
+  withFee &
+  withNet &
+  withItemInfo &
+  withItemAmount &
+  withInvoice &
+  withCustom &
+  withExtra &
+  withCommon &
+  withBuyerInfoFull &
+  withBuyerShippingAddress;
+
+type DonationIPN = BaseIPN & { type: IPN_TYPES.DONATION } & with2Currency &
+  withSubtotal &
+  withSingleItemOptions &
+  withShippingFee &
+  withTax &
+  withFee &
+  withNet &
+  withItemInfo &
+  withInvoice &
+  withCustom &
+  withExtra &
+  withCommon &
+  withBuyerInfoFull &
+  withBuyerShippingAddress;
+
+type CartIPN = BaseIPN & { type: IPN_TYPES.CART } & with2Currency &
+  withSubtotal &
+  withShippingFee &
+  withTax &
+  withFee &
+  withInvoice &
+  withCustom &
+  withExtra &
+  withCommon &
+  withBuyerInfoFull &
+  withBuyerShippingAddress &
+  NodeJS.Dict<string>; // item_name_#...
+
+type ApiIPN = BaseIPN & { type: IPN_TYPES.API } & with2Currency &
+  withFee &
+  withBuyerInfo &
+  withItemInfo &
+  withInvoice &
+  withCustom &
+  withCommon;
+
+type DepositIPN = BaseIPN & { type: IPN_TYPES.DEPOSIT } & withCurrency & {
+    dest_tag?: string;
+    confirms: number;
+    fee?: string; // Only positive
+    feei?: string; // Only positive
+    fiat_coin: string; // The ticker code of the fiat currency you selected on the Merchant Settings tab of the Account Settings page (USD, EUR, etc.) Make sure to check this for accuracy for security in your IPN handler! Huh?
+    fiat_amount: string;
+    fiat_amounti: string;
+    fiat_fee?: string; // Only positive
+    fiat_feei?: string; // Only positive
+    label?: string;
+  };
+
+type WithdrawalIPN = BaseIPN & { type: IPN_TYPES.WITHDRAWAL } & withCurrency & {
+    id: string;
+  };
+
+export {
+  SimpleIPN,
+  ButtonIPN,
+  DonationIPN,
+  CartIPN,
+  ApiIPN,
+  DepositIPN,
+  WithdrawalIPN,
+};
