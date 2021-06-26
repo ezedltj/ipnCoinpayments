e3e8d055599f7879984232f219139b5bb8a61b0e
OrahKokos <orah@kokos.tech>
2021-06-26 15:00:17 +0000

    ongoing type refinment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import crypto from 'crypto';
 import qs from 'querystring';
+import { ApiIPN, ApiIPNLike } from 'types/api-ipn.types';
+import { ButtonIPN, ButtonIPNLike } from 'types/button-ipn.types';
+import { CartIPN, CartIPNLike } from 'types/cart-ipn.types';
+import { IPNType, IPN_TYPES, IPN_DATA } from 'types/common.types';
+import { DepositIPN, DepositIPNLike } from 'types/deposit-ipn.types';
+import { DonationIPN, DonationIPNLike } from 'types/donation-ipn.types';
+import { SimpleIPN, SimpleIPNLike } from 'types/simple-ipn.types';
+import { UnknownIPN } from 'types/unknown-ipn.types';
+import { WithdrawalIPN, WithdrawalIPNLike } from 'types/withdrawal-ipn.types';
 import CoinpaymentsIPNError from './error';
 import {
   CoinpaymentsIPNLike,
-  IPN_DATA,
-  IPN_TYPES,
   CoinpaymentsIPN,
-  IPNType,
-  SimpleIPN,
+  processIPNTuple,
 } from './types/index.types';
 
 const parseObject = (obj: unknown): NodeJS.Dict<unknown> | undefined => {
@@ -57,16 +63,17 @@ const parseIPNLike = (ipnData: unknown): CoinpaymentsIPNLike | undefined => {
     return undefined;
   if (!ipnObj.hasOwnProperty('ipn_id')) return undefined;
   if (!ipnObj.hasOwnProperty('merchant')) return undefined;
+  if (!ipnObj.hasOwnProperty('type')) return undefined;
 
   const typeString = parseString(ipnObj.type);
   if (!typeString) return undefined;
-  const type = parseIPNType(typeString);
-  if (!type) return undefined;
+  const validIpnType = parseIPNType(typeString);
+  if (!validIpnType) return undefined;
 
   return ipnObj as CoinpaymentsIPNLike;
 };
 
-const verify = (
+const verifyIPN = (
   hmac: string,
   ipnSecret: string,
   ipnBody: CoinpaymentsIPNLike,
@@ -91,40 +98,54 @@ const processIPN = (
   hmac: string,
   ipnSecret: string,
   ipnData: unknown,
-): CoinpaymentsIPN => {
+): processIPNTuple => {
   const ipnLike = parseIPNLike(ipnData);
   if (!ipnLike) throw new CoinpaymentsIPNError('Invalid IPN data', ipnData);
 
-  const verified = verify(hmac, ipnSecret, ipnLike);
+  const verified = verifyIPN(hmac, ipnSecret, ipnLike);
   if (!verified)
     throw new CoinpaymentsIPNError('Unverifiable IPN data', ipnLike);
 
+  const ipn = parseIPN(ipnLike);
+};
+
+const parseIPN = (ipnLike: CoinpaymentsIPNLike): processIPNTuple => {
   switch (ipnLike.type) {
     case IPN_TYPES.SIMPLE:
-      return parseSimpleIPN(ipnLike);
+      return parseSimpleIPN(ipnLike as SimpleIPNLike);
     case IPN_TYPES.BUTTON:
-      return parseButtonIPN(ipnLike);
+      return parseButtonIPN(ipnLike as ButtonIPNLike);
     case IPN_TYPES.DONATION:
-      return parseDonationIPN(ipnLike);
+      return parseDonationIPN(ipnLike as DonationIPNLike);
     case IPN_TYPES.CART:
-      return parseCartIPN(ipnLike);
+      return parseCartIPN(ipnLike as CartIPNLike);
     case IPN_TYPES.API:
-      return parseApiIPN(ipnLike);
+      return parseApiIPN(ipnLike as ApiIPNLike);
     case IPN_TYPES.DEPOSIT:
-      return parseDepositIPN(ipnLike);
+      return parseDepositIPN(ipnLike as DepositIPNLike);
     case IPN_TYPES.WITHDRAWAL:
-      return parseWithdrawalIPN(ipnLike);
-    default:
-      return undefined;
+      return parseWithdrawalIPN(ipnLike as WithdrawalIPNLike);
+    default: {
+      return [
+        new CoinpaymentsIPNError('Unknown IPN Type', ipnLike),
+        ipnLike as UnknownIPN,
+      ];
+    }
   }
 };
 
-const parseSimpleIPN = (ipnLike: CoinpaymentsIPNLike): SimpleIPN => {};
-const parseButtonIPN = (ipnLike: CoinpaymentsIPNLike): ButtonIPN => {};
-const parseDonationIPN = (ipnLike: CoinpaymentsIPNLike): DonationIPN => {};
-const parseCartIPN = (ipnLike: CoinpaymentsIPNLike): CartIPN => {};
-const parseApiIPN = (ipnLike: CoinpaymentsIPNLike): ApiIPN => {};
-const parseDepositIPN = (ipnLike: CoinpaymentsIPNLike): DepositIPN => {};
-const parseWithdrawalIPN = (ipnLike: CoinpaymentsIPNLike): WithdrawalIPN => {};
+const parseSimpleIPN = (ipnLike: SimpleIPNLike): processIPNTuple => {};
+
+const parseButtonIPN = (ipnLike: ButtonIPNLike): processIPNTuple => {};
+
+const parseDonationIPN = (ipnLike: DonationIPNLike): processIPNTuple => {};
+
+const parseCartIPN = (ipnLike: CartIPNLike): processIPNTuple => {};
+
+const parseApiIPN = (ipnLike: ApiIPNLike): processIPNTuple => {};
+
+const parseDepositIPN = (ipnLike: DepositIPNLike): processIPNTuple => {};
+
+const parseWithdrawalIPN = (ipnLike: WithdrawalIPNLike): processIPNTuple => {};
 
 export { processIPN };
